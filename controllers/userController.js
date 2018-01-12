461316d08fe8d412b47e2f9b3526f1d7648d4edd
chadkeeven <chadkeeven@gmail.com>
2018-01-12 11:02:07 -0700

    adding user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,25 @@ const router = express.Router();
 
  //Home page
 router.get('/', function homepage(req, res) {
-  res.send("hi");
+  res.json("homePage");
 });
+
+//NEW user page
+router.get('/user/new', function newUser(req,res){
+	res.send("new user page");
+});
+
+//CREATE 
+
+
+//User account page
+
+//Create Search page
+
+//Edit search query page
+
+//Delete search query
+
 
 // //Add new cargo
 // router.post('/cargo', cargoController.create);
