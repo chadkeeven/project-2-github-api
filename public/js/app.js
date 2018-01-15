442713948a08f5ca9a150c3cd986db96b20a9fc4
chadkeeven <chadkeeven@gmail.com>
2018-01-15 16:32:33 -0700

    search by github username functionality completed

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,27 @@ $(document).ready(function() {
 		//console.log(userNameToSearch);
 		var userNameURL = url + userNameToSearch;
 		console.log(userNameURL);
-    	$.get(userNameURL, function(json, status){
-          // renderAlbum(json[json.length - 1]);
-        });
+		renderResults(userNameURL);
+    	// $.get(userNameURL, function(json, status){
+     //      // renderAlbum(json[json.length - 1]);
+     //    });
   });
+	//Function that renders results and places results on page
+	function renderResults(githubSearch){
+		$.get(githubSearch, function(json){
+			console.log(json);
+			var resultsHTML = 
+			"<!-- result -->" +
+			"<div class= 'row'>" +
+			"<ul class='list-group>" +
+			" <li class='list-group-item'>" +
+			"<a href=' "+ json.html_url + "'>" +
+			 json.html_url +
+			 "</a>" +
+            "</li>" +
+			"</ul>" + 
+			"</div>";
+			$(resultsHTML).appendTo('#searchResults');
+		});
+	}
 });
