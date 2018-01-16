4c6791ebb0189e2ee94dc81007a14bdb3d11ffc2
chadkeeven <chadkeeven@gmail.com>
2018-01-15 18:33:35 -0700

    converting bytes to percents on repos

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
 		//console.log(reposURL);
 		renderResults(reposURL);
 	});
+	var totalBytes = 0;
+	var languageArr=[];
 	//Function that renders results and places results on page
 	function renderResults(githubSearch){
 		$.get(githubSearch, function(json){
@@ -18,19 +20,27 @@ $(document).ready(function() {
 			//console.log(json[0].languages_url);
 			var repoLanguageURL = json[2].languages_url;
 			$.get(repoLanguageURL, function(json){
-				console.log(json);
+				//console.log(json);
 				var stringJson = JSON.stringify(json);
-				var replacedJson = stringJson.replace("{","");
+				var replacedJson = stringJson.replace(/{|}/g,"");
 				//console.log(stringJson);
 				var lanArr = replacedJson.split(",");
-				console.log(lanArr);
-				var totalBytes = 0;
-				// lanArr.forEach(function(lang, index){
-
-				// 	console.log(lang);
-				// });
 				//console.log(lanArr);
-				//var parsedJson= $.parseJSON(json);
+				lanArr.forEach(function(lang, index){
+					console.log(lang);
+					var splitLang = lang.split(":");
+					var langObj = {
+						language : splitLang[0],
+						bytes : parseInt(splitLang[1])
+					};
+					languageArr.push(langObj);
+					totalBytes += langObj.bytes;
+					//console.log("language:" +langObj.language);
+					//console.log("bytes:" +langObj.bytes);
+				});
+				//console.log(languageArr);
+				//console.log(totalBytes);
+				bytesToPercent(languageArr);
 				var resultsHTML = 
 				"<!-- result -->" +
 				"<div class= 'row'>" +
@@ -46,4 +56,20 @@ $(document).ready(function() {
 			});
 		});
 	}
+	//Function that turns bytes into percents
+	function bytesToPercent(arrayOfLanguages){
+		arrayOfLanguages.forEach(function(lang, index){
+			console.log(lang.bytes);
+			var bytePercent = (lang.bytes / totalBytes) * 100;
+			var roundedPercent = bytePercent.toFixed(2);
+			console.log(roundedPercent);
+		});
+	}
+
+
+
+
+
+
+
 });
