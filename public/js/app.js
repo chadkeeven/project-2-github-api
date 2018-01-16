95c7bb6469515f36dd649d13f42ba6e9f8c7b6c4
chadkeeven <chadkeeven@gmail.com>
2018-01-15 17:02:42 -0700

    working on returning languages on single repo

--- public/js/app.js
+++ public/js/app.js
@@ -7,28 +7,35 @@ $(document).ready(function() {
 		var userNameToSearch = $("#searchBox").val();
 		//console.log(userNameToSearch);
 		var userNameURL = url + userNameToSearch;
-		console.log(userNameURL);
-		renderResults(userNameURL);
-    	// $.get(userNameURL, function(json, status){
-     //      // renderAlbum(json[json.length - 1]);
-     //    });
-  });
+		const reposURL = userNameURL + "/repos";
+		//console.log(reposURL);
+		renderResults(reposURL);
+	});
 	//Function that renders results and places results on page
 	function renderResults(githubSearch){
 		$.get(githubSearch, function(json){
-			console.log(json);
-			var resultsHTML = 
-			"<!-- result -->" +
-			"<div class= 'row'>" +
-			"<ul class='list-group>" +
-			" <li class='list-group-item'>" +
-			"<a href=' "+ json.html_url + "'>" +
-			 json.html_url +
-			 "</a>" +
-            "</li>" +
-			"</ul>" + 
-			"</div>";
-			$(resultsHTML).appendTo('#searchResults');
+			//console.log(json[0]);
+			//console.log(json[0].languages_url);
+			var repoLanguageURL = json[2].languages_url;
+			$.get(repoLanguageURL, function(json){
+				var stringJson = JSON.stringify(json);
+				console.log(json);
+				var lanArr = json.split(",");
+				console.log(lanArr);
+				//var parsedJson= $.parseJSON(json);
+				var resultsHTML = 
+				"<!-- result -->" +
+				"<div class= 'row'>" +
+				"<ul class='list-group>" +
+				" <li class='list-group-item'>" +
+				//"<a href=' "+ json[0].html_url + "'>" +
+				json +
+				//"</a>" +
+				"</li>" +
+				"</ul>" + 
+				"</div>";
+				$(resultsHTML).appendTo('#searchResults');
+			});
 		});
 	}
 });
