262fff5f0fac267618b7433b0abaf292c2416ae1
chadkeeven <chadkeeven@gmail.com>
2018-01-16 10:58:40 -0700

    debugging findIndex in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 	var langObj={};
 	var indexOfLanguage;
 
+	//reset languageArr, languagesUsed after every submit
+
 	$("#usernameSearch").submit(function(event){
 		event.preventDefault();
 		var userNameToSearch = $("#searchBox").val();
@@ -52,47 +54,63 @@ $(document).ready(function() {
 	//function that gets the JSON in the language url
 	function getRepoLanguageJson(repoLanguageURL){
 		$.get(repoLanguageURL, function(json){
-			console.log(repoLanguageURL);
-			console.log(json);
+			//console.log(repoLanguageURL);
+			//console.log(json);
 			var stringJson = JSON.stringify(json);
-			console.log(stringJson == "{}");
+			//console.log(stringJson == "{}");
 			if (stringJson != "{}") {
 				var replacedJson = stringJson.replace(/{|}/g,"");
-				console.log(replacedJson);
-				var lanArr = replacedJson.split(",");
-				console.log("New lanArr: "+lanArr);
-				lanArr.forEach(function(lang, index){
-					var splitLang = lang.split(":");
-					if (languagesUsed.length > 0) {
-						if(languageAlreadyAdded(splitLang[0])){
+			//	console.log(replacedJson);
+			var lanArr = replacedJson.split(",");
+			//	console.log("New lanArr: "+lanArr);
+			lanArr.forEach(function(lang, index){
+				var splitLang = lang.split(":");
+				//console.log(splitLang[0]);
+				//console.log( " languagesUsed are: "+ languagesUsed);
+				//If languages have been added to array we want to test to
+				//see if current splitLang[0] has been used
+				if (languagesUsed.length > 0) {
+					//console.log(languageAlreadyAdded(splitLang[0]));
+
+					//if languageAlreadyAdded returns true we still want to add
+					//the value of the bytes to the langguage it matches in the languageArr
+					if(languageAlreadyAdded(splitLang[0])){
 					//console.log(languageArr[indexOfLanguage].amountOfLanguage);
-					languageArr[indexOfLanguage].amountOfLanguage += parseInt(splitLang[1]);
+						languageArr[indexOfLanguage].amountOfLanguage += parseInt(splitLang[1]);
+					//if language hasn't been added we want to make a langObj
+					//and push into languageArr
+					}else{
+						langObj = {
+							language : splitLang[0],
+							amountOfLanguage : parseInt(splitLang[1])
+						};
+						languageArr.push(langObj);
+						//console.log(languageArr);
+						languagesUsed.push(splitLang[0]);
+					}
+				//if no languages have been used we want to create a langObj
+				//push into languageArr and push the language into languagesUsed	
 				}else{
 					langObj = {
 						language : splitLang[0],
 						amountOfLanguage : parseInt(splitLang[1])
 					};
-
 					languageArr.push(langObj);
+					//console.log(languageArr);
 					languagesUsed.push(splitLang[0]);
-				}
-			}else{
-				langObj = {
-					language : splitLang[0],
-					amountOfLanguage : parseInt(splitLang[1])
-				};
-				languageArr.push(langObj);
-				languagesUsed.push(splitLang[0]);
 			}
 		});
-			}
-		});
+		}
+	});
 	}
 
 	//function that checks to see if language has already been added
 	//to master array
 	function languageAlreadyAdded(languageToCheck){
 		function something(language){
+			console.log("Language to Check: "+languageToCheck);
+			console.log("Language: "+ language);
+			console.log(languageToCheck == language);
 			return  languageToCheck == language;
 		}
 		if (languagesUsed.findIndex(something) === -1) {
