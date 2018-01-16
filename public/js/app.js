2780f08eb55af4ad33c632927c41c90e3874359b
chadkeeven <chadkeeven@gmail.com>
2018-01-16 10:27:25 -0700

    debugging app.js for github api

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
 	const url = "https://api.github.com/users/";
+	var totalBytes = 0;
+	var languageArr=[];
+	var languagesUsed =[];
+	//var languageFound = false;
+	var langObj={};
+	var indexOfLanguage;
 
 	$("#usernameSearch").submit(function(event){
 		event.preventDefault();
@@ -8,68 +14,144 @@ $(document).ready(function() {
 		//console.log(userNameToSearch);
 		var userNameURL = url + userNameToSearch;
 		const reposURL = userNameURL + "/repos";
-		//console.log(reposURL);
+		//const sortByCreateURL = reposURL + "?sort=created";
 		renderResults(reposURL);
+		addResults(languageArr);
+		//setTimeout(addResults(), 3000);
 	});
-	var totalBytes = 0;
-	var languageArr=[];
+
 	//Function that renders results and places results on page
 	function renderResults(githubSearch){
+		getApiRequest(githubSearch);	
+
+	}
+
+	//Function that gets api request
+	function getApiRequest(githubSearch){
 		$.get(githubSearch, function(json){
-			//console.log(json[0]);
-			//console.log(json[0].languages_url);
-			var repoLanguageURL = json[2].languages_url;
-			$.get(repoLanguageURL, function(json){
-				//console.log(json);
-				var stringJson = JSON.stringify(json);
-				var replacedJson = stringJson.replace(/{|}/g,"");
-				//console.log(stringJson);
-				var lanArr = replacedJson.split(",");
-				//console.log(lanArr);
-				lanArr.forEach(function(lang, index){
-					console.log(lang);
-					var splitLang = lang.split(":");
-					var langObj = {
-						language : splitLang[0],
-						bytes : parseInt(splitLang[1])
-					};
-					languageArr.push(langObj);
-					totalBytes += langObj.bytes;
-					//console.log("language:" +langObj.language);
-					//console.log("bytes:" +langObj.bytes);
-				});
-				//console.log(languageArr);
-				//console.log(totalBytes);
-				bytesToPercent(languageArr);
-				var resultsHTML = 
-				"<!-- result -->" +
-				"<div class= 'row'>" +
-				"<ul class='list-group>" +
-				" <li class='list-group-item'>" +
-				//"<a href=' "+ json[0].html_url + "'>" +
-				json +
-				//"</a>" +
-				"</li>" +
-				"</ul>" + 
-				"</div>";
-				$(resultsHTML).appendTo('#searchResults');
-			});
+			getRepo(json);
+		});
+	} 
+
+	//Function that goes through each repo
+	function getRepo(json){
+		// for (var i = 0; i < 1; i++) {
+		// 	console.log(json[i].name);
+		// 	var repoLanguageURL = json[i].languages_url;
+		// 	console.log(repoLanguageURL);
+		// 	getRepoLanguageJson(repoLanguageURL);
+		// }
+		json.forEach(function(repo,index){
+			//console.log(json[index].name);
+			var repoLanguageURL = json[index].languages_url;
+			//console.log(repoLanguageURL);
+			getRepoLanguageJson(repoLanguageURL);
 		});
 	}
+
+	//function that gets the JSON in the language url
+	function getRepoLanguageJson(repoLanguageURL){
+		$.get(repoLanguageURL, function(json){
+			console.log(repoLanguageURL);
+			console.log(json);
+			var stringJson = JSON.stringify(json);
+			console.log(stringJson == "{}");
+			if (stringJson != "{}") {
+				var replacedJson = stringJson.replace(/{|}/g,"");
+				console.log(replacedJson);
+				var lanArr = replacedJson.split(",");
+				console.log("New lanArr: "+lanArr);
+				lanArr.forEach(function(lang, index){
+					var splitLang = lang.split(":");
+					if (languagesUsed.length > 0) {
+						if(languageAlreadyAdded(splitLang[0])){
+					//console.log(languageArr[indexOfLanguage].amountOfLanguage);
+					languageArr[indexOfLanguage].amountOfLanguage += parseInt(splitLang[1]);
+				}else{
+					langObj = {
+						language : splitLang[0],
+						amountOfLanguage : parseInt(splitLang[1])
+					};
+
+					languageArr.push(langObj);
+					languagesUsed.push(splitLang[0]);
+				}
+			}else{
+				langObj = {
+					language : splitLang[0],
+					amountOfLanguage : parseInt(splitLang[1])
+				};
+				languageArr.push(langObj);
+				languagesUsed.push(splitLang[0]);
+			}
+		});
+			}
+		});
+	}
+
+	//function that checks to see if language has already been added
+	//to master array
+	function languageAlreadyAdded(languageToCheck){
+		function something(language){
+			return  languageToCheck == language;
+		}
+		if (languagesUsed.findIndex(something) === -1) {
+			return false;
+		}else{
+			indexOfLanguage = languagesUsed.findIndex(something);
+			//console.log(languagesUsed.findIndex(something));
+			return true;
+		}
+	}
+
+	//function that appends results to page
+	function addResults(languageArr){
+		//console.log(languageArr);
+		bytesToPercent(languageArr);
+		languageArr.forEach(function(program, index){
+			var resultsHTML = 
+			"<!-- result -->" +
+			"<div class= 'row'>" +
+			"<ul class='list-group>" +
+			" <li class='list-group-item'>" +
+			program.language +
+			": " +
+			program.amountOfLanguage +
+			"%" +
+			"</li>" +
+			"</ul>" + 
+			"</div>";
+			$(resultsHTML).appendTo('#searchResults');
+		});
+	}
+
+
+ 						//totalBytes += langObj.amountOfLanguage;
+// 						console.log(totalBytes);
+	//function that adds all bytes up
+	function addBytes(arrayWithBytes){
+		arrayWithBytes.forEach(function(lang,index){
+			if(isNaN(lang.amountOfLanguage)){
+				arrayWithBytes.splice(index,1);
+			}else{
+				totalBytes += lang.amountOfLanguage;
+				console.log(totalBytes);
+			}
+		});
+	}
+
+
+
 	//Function that turns bytes into percents
 	function bytesToPercent(arrayOfLanguages){
+		addBytes(arrayOfLanguages);
 		arrayOfLanguages.forEach(function(lang, index){
-			console.log(lang.bytes);
-			var bytePercent = (lang.bytes / totalBytes) * 100;
+			//console.log(lang.amountOfLanguage);
+			var bytePercent = (lang.amountOfLanguage / totalBytes) * 100;
+			//console.log(bytePercent);
 			var roundedPercent = bytePercent.toFixed(2);
-			console.log(roundedPercent);
+			//console.log(roundedPercent);
+			lang.amountOfLanguage = roundedPercent;
 		});
 	}
-
-
-
-
-
-
-
 });
